53537b1c5c213db26475879c997a542cea971843
wolfx <digimonx@qq.com>
2019-06-14 18:24:39 +0800

    vue-wangeditor demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div ref="container"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  mounted() {
+    const editor = new window.wangEditor(this.$refs.container);
+    editor.customConfig.uploadImgServer = "/upload";
+    editor.customConfig.uploadImgMaxSize = 5 * 1024 * 1024; // 5m
+    editor.customConfig.uploadImgParams = {
+      // 如果版本 <=v3.1.0 ，属性值会自动进行 encode ，此处无需 encode
+      // 如果版本 >=v3.1.1 ，属性值不会自动 encode ，如有需要自己手动 encode
+      token: "abcdef12345"
+    };
+    editor.customConfig.uploadImgParamsWithUrl = true;
+    editor.customConfig.uploadFileName = "file";
+    editor.customConfig.uploadImgHooks = {
+      before: function(xhr, editor, files) {},
+      success: function(xhr, editor, result) {},
+      fail: function(xhr, editor, result) {},
+      error: function(xhr, editor) {},
+      timeout: function(xhr, editor) {},
+      customInsert: function(insertImg, result, editor) {
+        const url = result.url;
+        insertImg(url);
+      }
+    };
+    editor.create();
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
