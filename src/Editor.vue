a72828390f2484d102f69b672485e30942c8633c
wolfx <digimonx@qq.com>
2019-06-17 11:12:42 +0800

    优化

--- src/Editor.vue
+++ src/Editor.vue
@@ -3,77 +3,100 @@
 </template>
 
 <script>
-// https://www.kancloud.cn/wangfupeng/wangeditor3/332599
-function parseDom(str) {
-  const ele = document.createElement("div");
-  ele.innerHTML = str;
-  return ele.childNodes;
-}
-export default {
-  name: "Editor",
-  props: ["value", "mobileFilter"],
-  mounted() {
-    this.editor = new window.wangEditor(this.$refs.container);
-    this.editor.customConfig.uploadImgServer = "/upload?token=123";
-    this.editor.customConfig.uploadImgMaxSize = 5 * 1024 * 1024; // 5m
-    this.editor.customConfig.onchange = html => {
-      // html 即变化之后的内容
-      this.$emit("input", html);
-    };
-    this.editor.customConfig.uploadFileName = "file";
-    this.editor.customConfig.uploadImgHooks = {
-      before: function(xhr, editor, files) {},
-      success: function(xhr, editor, result) {},
-      fail: function(xhr, editor, result) {},
-      error: function(xhr, editor) {},
-      timeout: function(xhr, editor) {},
-      customInsert: function(insertImg, result, editor) {
-        const url = result.url;
-        insertImg(url);
+  // https://www.kancloud.cn/wangfupeng/wangeditor3/332599
+  function parseDom(str) {
+    const ele = document.createElement("div");
+    ele.innerHTML = str;
+    return ele.childNodes;
+  }
+  export default {
+    name: "Editor",
+    props: ["value", "mobileFilter", "contentHeight"],
+    mounted() {
+      this.editor = new window.wangEditor(this.$refs.container);
+      this.editor.customConfig.uploadImgServer = "/upload";
+      this.editor.customConfig.uploadImgMaxSize = 5 * 1024 * 1024; // 5m
+      // 自定义菜单配置
+      this.editor.customConfig.menus = [
+        // 'head',  // 标题
+        'bold',  // 粗体
+        'fontSize',  // 字号
+        // 'fontName',  // 字体
+        'italic',  // 斜体
+        'underline',  // 下划线
+        'strikeThrough',  // 删除线
+        'foreColor',  // 文字颜色
+        // 'backColor',  // 背景颜色
+        'link',  // 插入链接
+        'justify',  // 对齐方式
+        'image',  // 插入图片
+        'undo',  // 撤销
+        'redo'  // 重复
+      ]
+      this.editor.customConfig.onchange = html => {
+        // html 即变化之后的内容
+        if (this.mobileFilter) {
+          const dom = parseDom("<div>" + html + "</div>");
+          html = this.filterMobileImage(dom);
+        }
+        this.$emit("input", html);
+      };
+      this.editor.customConfig.uploadFileName = "file";
+      this.editor.customConfig.uploadImgHooks = {
+        before: (xhr, editor, files) => {},
+        success: (xhr, editor, result) => {},
+        fail: (xhr, editor, result) => {},
+        error: (xhr, editor) => {},
+        timeout: (xhr, editor) => {},
+        customInsert: (insertImg, result, editor) => {
+          const url = window.SITE_CONFIG["resourceServer"] + result.picPath;
+          insertImg(url);
+        },
+      };
+      // 自定义处理粘贴的文本内容
+      this.editor.customConfig.pasteTextHandle = content => {
+        // content 即粘贴过来的内容（html 或 纯文本），可进行自定义处理然后返回
+        if (this.mobileFilter) {
+          // 过滤样式适配手机
+          const dom = parseDom("<div class='paste'>" + content + "</div>");
+          return this.filterMobileImage(dom);
+        } else {
+          return content;
+        }
+      };
+      this.editor.create();
+      if (this.contentHeight) {
+        const container = document.querySelector(".w-e-text-container");
+        container.style.height = this.contentHeight;
       }
-    };
-    // 自定义处理粘贴的文本内容
-    this.editor.customConfig.pasteTextHandle = function(content) {
-      // content 即粘贴过来的内容（html 或 纯文本），可进行自定义处理然后返回
-      if (this.mobileFilter) {
-        // 过滤样式适配手机
-        const dom = parseDom("<div class='paste'>" + content + "</div>");
+    },
+    watch: {
+      value: {
+        handler(newValue) {
+          this.setContent(newValue);
+        },
+        immediate: false,
+      },
+    },
+    methods: {
+      filterMobileImage(dom) {
         const imageList = dom[0].querySelectorAll("img");
         imageList.forEach(image => {
           // 去掉图片的固定宽度，改为 100%
-          const w = image.width || image.style.width;
-          const h = image.height || image.style.height;
-          if (w) {
-            image.setAttribute("o-width", w);
-            image.setAttribute("o-height", h);
-            image.setAttribute("width", "100%");
-            image.removeAttribute("height");
-          }
+          image.removeAttribute("width");
+          image.removeAttribute("height");
+          image.style.width = "100%";
         });
-        return dom[0].outerHTML;
-      } else {
-        return content;
-      }
-    };
-    this.editor.create();
-  },
-  watch: {
-    value: {
-      handler(newValue) {
-        this.setContent(newValue);
+        return dom[0].innerHTML;
       },
-      immediate: false
-    }
-  },
-  methods: {
-    setContent(html) {
-      this.editor.txt.html(html);
+      setContent(html) {
+        this.editor.txt.html(html);
+      },
+      getContent() {
+        this.editor.txt.html();
+      },
     },
-    getContent() {
-      this.editor.txt.html();
-    }
-  }
-};
+  };
 </script>
 
 <style scoped></style>
